9426a24f227cc76f8827b97837d8f026f25c0613
JoshuaToback <joshuatoback@gmail.com>
2022-12-19 19:52:55 -0500

    Added seeds.sql and menu items

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS museum_db;
+DROP DATABASE IF EXISTS orders_db;
 
-CREATE DATABASE museum_db;
+CREATE DATABASE orders_db;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE orders_db;
+INSERT INTO department (name) VALUES ('Research and Development');
+INSERT INTO role (title, salary, department_id) VALUES ('Engineer', 75000.00, 1);
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES ('Joshua', 'Toback', 17, 1);
+
+
+-- Menu Items: Eggs, Bacon, Avacado Toast, Biscuits, Coffee, Milk 
