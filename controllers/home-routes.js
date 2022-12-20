5fe63bc8f469763766f3112937dfa3f9ed8d8055
JoshuaToback <joshuatoback@gmail.com>
2022-12-19 19:43:43 -0500

    Added file structure

---
+++ controllers/home-routes.js
@@ -0,0 +1,94 @@
+const router = require('express').Router();
+const { Gallery, Painting } = require('../models');
+// TODO: Import the custom middleware
+
+// GET all galleries for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbGalleryData = await Gallery.findAll({
+      include: [
+        {
+          model: Painting,
+          attributes: ['filename', 'description'],
+        },
+      ],
+    });
+
+    const galleries = dbGalleryData.map((gallery) =>
+      gallery.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+// TODO: Replace the logic below with the custom middleware
+router.get('/gallery/:id', async (req, res) => {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, allow them to view the gallery
+    try {
+      const dbGalleryData = await Gallery.findByPk(req.params.id, {
+        include: [
+          {
+            model: Painting,
+            attributes: [
+              'id',
+              'title',
+              'artist',
+              'exhibition_date',
+              'filename',
+              'description',
+            ],
+          },
+        ],
+      });
+      const gallery = dbGalleryData.get({ plain: true });
+      res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+// GET one painting
+// TODO: Replace the logic below with the custom middleware
+router.get('/painting/:id', async (req, res) => {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, allow them to view the painting
+    try {
+      const dbPaintingData = await Painting.findByPk(req.params.id);
+
+      const painting = dbPaintingData.get({ plain: true });
+
+      res.render('painting', { painting, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
---
+++ utils/auth.js
@@ -0,0 +1,6 @@
+const withAuth = (req, res, next) => {
+  // TODO: If the user is not logged in, redirect the user to the login page
+  // TODO: If the user is logged in, allow them to view the paintings
+};
+
+module.exports = withAuth;
