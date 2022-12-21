41a306b3fdb8f60358aa91d715f1ef9f7b9ca80d
jjohnson673 <jkjohnson673@gmail.com>
2022-12-20 19:05:57 -0500

    worked through/cleaned some of the models and db files

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+
+//fix const below when home-routes.js is fully updated
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
--- models/menu.js
+++ models/menu.js
@@ -4,3 +4,28 @@ const sequelize = require('../config/connection');
 class Item extends Model {
 
 };
+
+Item.init(
+    {
+      order_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      item_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+
+      },
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        },
+      });
+    
