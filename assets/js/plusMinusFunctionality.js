53c18cbb017fb84412bd431201fd16f83542d9ba
Josh Icard <joshicard@Joshs-MacBook-Pro.local>
2023-01-07 21:03:38 -0500

    math

--- assets/js/plusMinusFunctionality.js
+++ assets/js/plusMinusFunctionality.js
@@ -1,5 +1,5 @@
 var minusCoffee = document.getElementById('minus-coffee-button');
-var plusCoffee = document.getElementById('coffee');
+var plusCoffee = document.getElementById('plus-coffee-button');
 var minusMilk = document.getElementById('minus-milk-button');
 var plusMilk = document.getElementById('plus-milk-button');
 var minusPancakes = document.getElementById('minus-pancakes-button');
@@ -7,6 +7,7 @@ var plusPancakes = document.getElementById('plus-pancakes-button');
 var minusBacon = document.getElementById('minus-bacon-button');
 var plusBacon = document.getElementById('plus-bacon-button');
 var checkoutButton = document.getElementById('checkout-button');
+var goToCheckoutButton = document.getElementById('goToCheckout');
 
 //plusCoffee button on click calls addCoffee
 //addCoffee adds one instance of 'coffee' when + is clicked
@@ -16,8 +17,10 @@ plusCoffee.addEventListener('click', addCoffee);
 function addCoffee() {
     var customerOrder = JSON.parse(localStorage.getItem("customerOrder")) || [];
     var item = 'Coffee';
+    var cost = 1.99;
     var menuItem = {
-        name: item
+        name: item,
+        price: cost
     }
     customerOrder.push(menuItem);
     localStorage.setItem('customerOrder', JSON.stringify(customerOrder));
@@ -26,23 +29,26 @@ function addCoffee() {
 minusCoffee.addEventListener('click', subtractCoffee);
 function subtractCoffee() {
     var customerOrder = JSON.parse(localStorage.getItem("customerOrder"));
-    var index = customerOrder.findIndex(o => o.name === 'coffee');
-    var removed = index !== -1 && customerOrder.splice(index, 1);
+    var index = customerOrder.findIndex(o => o.name == 'Coffee');
+    var removed = index ==! -1 && customerOrder.splice(index, 1);
     localStorage.setItem('customerOrder', JSON.stringify(customerOrder));
     updateOrderList();
 }
 
-//show order on page with l
+//show order on page with list
 function updateOrderList() {
 var orderList = document.querySelector('.orderList')
 var customerOrder = JSON.parse(localStorage.getItem("customerOrder"));
 let orderItems = '';
 for (let orderItemName of customerOrder) {
-    orderItems += '<li>' + orderItemName.name + '</li>';
+    orderItems += '<li>' + orderItemName.name + orderItemName.price + '</li>';
 }
 console.log(orderItems)
 orderList.innerHTML = orderItems;
 }
+
+window.onload = updateOrderList();
+
 
 
 
