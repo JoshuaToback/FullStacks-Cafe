5d1edfe0a97c94a89e3d829a6456bde66f9d82ce
Josh Icard <joshicard@Joshs-MacBook-Pro.local>
2023-01-09 18:31:20 -0500

    fixing schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,11 @@ create database FullStackCafe;
 
 use FullStackCafe;
 
+/*
 CREATE TABLE order_details (
   id INT AUTO_INCREMENT NOT NULL,
   dish_name VARCHAR(30) NULL,
   price DECIMAL (4,2) NULL,
   PRIMARY KEY (id)
 );
+*/
