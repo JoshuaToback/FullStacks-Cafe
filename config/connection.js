42a3d4a4f67616d8ee83c9521675ae43cdecaccf
Josh Icard <joshicard@Joshs-MacBook-Pro.local>
2023-01-09 17:57:53 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,14 @@
 const Sequelize = require('sequelize');
+const { rootCertificates } = require('tls');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  //process.env.DB_NAME,
-  //process.env.DB_USER,
-  //process.env.DB_PASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: 'cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host: 'localhost',
     dialect: 'mysql',
-    username: q7lpm35zgpf01cfy,
-    password: kxur0928bicctfc3,
-    port: 3306,
-    database: xklwzntiuqjor2ku
   }
 );
 
